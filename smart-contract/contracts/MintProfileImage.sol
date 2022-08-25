2d9c36c5b27d2ad36ffd8d1b21792cfc19c64e97
anhtudo97 <anhtudo97@gamil.com>
2022-08-25 22:28:13 +0700

    feat: update smart contract

--- smart-contract/contracts/MintProfileImage.sol
+++ smart-contract/contracts/MintProfileImage.sol
@@ -1,7 +1,3 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-// Import this file to use console.log
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -21,6 +17,10 @@ contract ProfileImageNfts is ERC721, Ownable {
 
     constructor() ERC721("ProfileImageNfts", "PIN") {}
 
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
     function tokenURI(uint256 tokenId)
         public
         view
@@ -31,5 +31,28 @@ contract ProfileImageNfts is ERC721, Ownable {
         require(_exists(tokenId), "URI not exist on that ID");
         string memory _RUri = _tokenURIs[tokenId];
         return _RUri;
+    }
+
+    function getAlltoken() public view returns (RenderToken[] memory) {
+        uint256 latestId = _tokenIds.current();
+        RenderToken[] memory res = new RenderToken[](latestId);
+        for (uint256 i = 0; i <= latestId; i++) {
+            if (_exists(i)) {
+                string memory uri = tokenURI(i);
+                res[i] = RenderToken(i, uri, " ");
+            }
+        }
+        return res;
+    }
+
+    function mint(address recipents, string memory _uri)
+        public
+        returns (uint256)
+    {
+        uint256 newId = _tokenIds.current();
+        _mint(recipents, newId);
+        _setTokenURI(newId, _uri);
+        _tokenIds.increment();
+        return newId;
     }
 }
